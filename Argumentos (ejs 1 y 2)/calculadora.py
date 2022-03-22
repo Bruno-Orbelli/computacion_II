95867dccbcf2484f9fc306fc54f3cdc20655af17
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-03-21 22:34:20 -0300

    Argumentos

--- Argumentos (ejs 1 y 2)/calculadora.py
+++ Argumentos (ejs 1 y 2)/calculadora.py
@@ -1,17 +1,26 @@
-import argparse
+import getopt
+import sys
 
-parser = argparse.ArgumentParser(description = 'Permite realizar las cuatro operaciones básicas (+, -, *, /) sobre dos operandos dados.')
-
-parser.add_argument('-o', '--operation', required = True, choices = ['+', '-', '/', '*'], help = 'Operación a realizar (+, -, *, /).')
-parser.add_argument('-n', required = True, type = int, help = 'Primer operando de la operación.')
-parser.add_argument('-m', required = True, type = int, help = 'Segundo operando de la operación.')
-args = parser.parse_args()
-
-try:
-    if args.operation == '/' and args.m == 0:
-        raise Exception('division by 0')
-    else:
-        x = '{} {} {}'.format(args.n, args.operation, args.m)
-        print('{} = {}'.format(x, eval(x)))
-except Exception as e:
-    print('calculadora.py: error: {}'.format(e))
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "o:n:m:", ["operacion", "op1", "op2"])
+    except getopt.GetoptError as e:
+        print('error: {}'.format(e))
+        sys.exit(404)
+    elementos = []
+    try:
+        for elem in opts:
+            if elem[0] in ('-n', '--op1'):
+                if type(elem[1]) != int:
+                    raise Exception('operando no entero ingresado')
+                else:
+                    elementos.insert(0, elem[1])
+            elif elem[0] in ('-o', '--operacion'):
+                if elem[1] not in ('+', '-', '*')
+                elementos.insert(1, elem[1])
+            else:
+                elementos.insert(2, elem[1])
+        ecuacion = '{} {} {}'.format(elementos[0], elementos[1], elementos[2])
+        print('{} = {}'.format(ecuacion, eval(ecuacion)))
+    except Exception as e:
+        print('error: {}'.format(e))
