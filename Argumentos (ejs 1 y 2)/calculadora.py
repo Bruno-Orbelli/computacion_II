445f61d5b7bf71060b378613a0fd58fcc77610b5
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-03-21 21:21:31 -0300

    Argumentos

--- Argumentos (ejs 1 y 2)/calculadora.py
+++ Argumentos (ejs 1 y 2)/calculadora.py
@@ -1,7 +1,15 @@
 import argparse
-import re
 
-parser = argparse.ArgumentParser(description = 'Permite realizar las cuatro operaciones básicas (+, -, *, /) sobre dos operandos dados.',
-exit_on_error = False)
-parser.add_argument('-o', required = True, choices = ['+', '-', '/', '*'], help = 'Operación a realizar (+, -, *, /).')
-parser.add_argument('-n', required = True, type = int, help = 'Primer operando de la operación')
+parser = argparse.ArgumentParser(description = 'Permite realizar las cuatro operaciones básicas (+, -, *, /) sobre dos operandos dados.')
+parser.add_argument('-o', '--operation', required = True, choices = ['+', '-', '/', '*'], help = 'Operación a realizar (+, -, *, /).')
+parser.add_argument('-n', required = True, type = int, help = 'Primer operando de la operación.')
+parser.add_argument('-m', required = True, type = int, help = 'Segundo operando de la operación.')
+args = parser.parse_args()
+try:
+    if args.operation == '/' and args.m == 0:
+        raise Exception('division by 0')
+    else:
+        x = '{} {} {}'.format(args.n, args.operation, args.m)
+        print('{} = {}'.format(x, eval(x)))
+except Exception as e:
+    print('calculadora.py: error: {}'.format(e))
