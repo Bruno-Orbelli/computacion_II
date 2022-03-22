fce9080e7c223e1e531f802d86fde7321ddc2a96
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-03-21 23:30:04 -0300

    Copiador final

--- Argumentos (ejs 1 y 2)/copiador.py
+++ Argumentos (ejs 1 y 2)/copiador.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description = '''Toma el nombre de dos archivos y copia el contenido de uno dentro del otro.
     Si el archivo de destino ya existe, procede a sobreescribir su contenido.''')
-    parser.add_argument('-i', '--inicial', help = 'Archivo original (del cual se copia el contenido)', required = True)
-    parser.add_argument('-o', '--final', help = 'Archivo destino (en el cual se copia/sobreescribe el contenido)', required = True)
+    parser.add_argument('-i', '--inicial', help = 'archivo original (del cual se copia el contenido)', required = True)
+    parser.add_argument('-o', '--final', help = 'archivo destino (en el cual se copia/sobreescribe el contenido)', required = True)
     args = parser.parse_args()
 
     try:
