195da5f0c64fcd0f54c60644c866c15599a80d19
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-03-21 23:27:06 -0300

    Argumentos

---
+++ Argumentos (ejs 1 y 2)/copiador.py
@@ -0,0 +1,17 @@
+import argparse
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description = '''Toma el nombre de dos archivos y copia el contenido de uno dentro del otro.
+    Si el archivo de destino ya existe, procede a sobreescribir su contenido.''')
+    parser.add_argument('-i', '--inicial', help = 'Archivo original (del cual se copia el contenido)', required = True)
+    parser.add_argument('-o', '--final', help = 'Archivo destino (en el cual se copia/sobreescribe el contenido)', required = True)
+    args = parser.parse_args()
+
+    try:
+        with open(args.inicial, 'r') as file1:
+            with open(args.final, 'w') as file2:
+                for x in file1:
+                    file2.write(x)
+    except Exception as e:
+        print('error: {}'.format(e))
