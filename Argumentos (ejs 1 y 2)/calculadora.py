af3decf691421ea8e33d3ed1cea54d9e511c311f
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-03-21 21:24:57 -0300

    Argumentos

--- Argumentos (ejs 1 y 2)/calculadora.py
+++ Argumentos (ejs 1 y 2)/calculadora.py
@@ -1,10 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser(description = 'Permite realizar las cuatro operaciones básicas (+, -, *, /) sobre dos operandos dados.')
+
 parser.add_argument('-o', '--operation', required = True, choices = ['+', '-', '/', '*'], help = 'Operación a realizar (+, -, *, /).')
 parser.add_argument('-n', required = True, type = int, help = 'Primer operando de la operación.')
 parser.add_argument('-m', required = True, type = int, help = 'Segundo operando de la operación.')
 args = parser.parse_args()
+
 try:
     if args.operation == '/' and args.m == 0:
         raise Exception('division by 0')
