03d129280fb7853d90401631e5281be44f4f7410
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2022-04-23 12:28:56 -0300

    Inversor casi terminado

--- Pipe/inversor.py
+++ Pipe/inversor.py
@@ -1,3 +1,4 @@
+from binhex import LINELEN
 import os, argparse
 
 def get_args():
@@ -11,34 +12,54 @@ def get_args():
 
 def main(args):
     
-    r, w = os.pipe()
+    rp, wp = os.pipe() # pipe del proc. padre
+    rc, wc = os.pipe() # pipe del proc. hijo
 
     with open(args.file, 'r+') as file:
         
+        invLines, linesAmount = 0, len(file.readlines())
+        file.seek(0)
+
         for i in file.readlines():
             
             line = i.strip()
-            
             new_proc = os.fork()
+            print(new_proc)
         
-            if not new_proc:
-                os.close(r)
-                w = os.fdopen(w, 'w')
-                w.write(f'{line[::-1]}\n')
-                w.close()
+            if new_proc:
+                if not invLines:                
+                    wc = os.fdopen(wc, 'w')
+                
+                wc.write(line)
+                invLines += 1
+                
+                if invLines == linesAmount:
+                   wc.close()
+            
+            else:
+                try:
+                    os.close(wc)
+                except TypeError:
+                    wc.close()
+                
+                rc = os.fdopen(rc, 'r')
+                toRead = rc.read()
+                rc.close()
+                
+                wp = os.fdopen(wp, 'w')
+                wp.write(f'{toRead[::-1]}\n')
+                wp.close()
                 os._exit(0)
             
-        file.seek(0)
-        
-        for j in range(len(file.readlines())):
+        for j in range(linesAmount):
             os.wait()
+            print('hijo terminado')
 
-        if os.getpid():
-            os.close(w)
-            r = os.fdopen(r, 'r')
-            for phrase in r.readlines():
-                print(f'{phrase}', end = '')
-            r.close()
+        os.close(wp)
+        rp = os.fdopen(rp, 'r')
+        for phrase in rp.readlines():
+            print(f'{phrase}')
+        rp.close()
 
 if __name__ == '__main__':
     main(get_args())
