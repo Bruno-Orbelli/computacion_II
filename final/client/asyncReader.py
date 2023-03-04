77c2d85f450f32c0102ab56bc0ea2ca71dce5d51
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2023-03-03 23:45:17 -0300

    Added SQL connections exceptions

--- final/client/asyncReader.py
+++ final/client/asyncReader.py
@@ -4,84 +4,126 @@ from asyncio import create_task, gather, run
 
 from queries import SQLDbStructureQueries, SQLdataQueries, mongodbAvailableQueryElems
 from connectionData import drivers, connStrs
+from exceptions import ExecutionError, ConnectionError, UnsupportedDBTypeError, ArgumentError
 
 # SQL
 
-async def get_connection(dbType: str, dbPath: str = None, additionalData: dict = None):
+async def get_connection(dbType: str, dbPath: str = None, additionalParams: dict = None):
+    
     if dbType not in connStrs:
-        # raise Exception -- "Database type 'dbType' is not currently supported."
-        return
+        raise UnsupportedDBTypeError(f"Unsupported or unexisting database type '{dbType}'.")
     else:
         connectionStr = connStrs[dbType]
         if dbType in ("mysql", "postgresql"):
-            connectionStr = connectionStr.format(**additionalData)
+            try:
+                connectionStr = connectionStr.format(**additionalParams)
+            except KeyError:
+                raise ArgumentError(f"Missing required arguments for establishing '{dbType}' connection.")
         elif dbType == "sqlite3":
-            connectionStr = connectionStr.format(dbPath)
+                connectionStr = connectionStr.format(dbPath)
+                process = Popen(["cat", dbPath], stdout= PIPE, stderr= PIPE)
+                if process.communicate()[1]:
+                    raise ArgumentError(f"Database path '{dbPath}' not found; check for any misspellings.")
     
     connectionStr = f"DRIVER={{{drivers[dbType]}}};" + connectionStr
-    return pyodbc.connect(connectionStr)
+    
+    try:
+        return pyodbc.connect(connectionStr, readonly= True, autocommit= False)
+    except (pyodbc.OperationalError, pyodbc.Error):
+        raise ConnectionError(
+            "Failed to establish connection with {host}:{port} (database `{dbName}`).\nEnsure: \n - server is running and accepting TCP/IP connections at that address. \n - the correct database type has been specified. \n - other requiered arguments (username, password, ...) are not incorrect.".format(**additionalParams)
+            )
     
 async def get_cursor(connObject):
-    return connObject.cursor()
+    try:
+        return connObject.cursor()
+    except pyodbc.Error:
+        raise ConnectionError("Connection with database at {host}:{port} (`{dbName}`) has been lost.")
 
-async def read_tables(dbType: str, dbPath: str, additionalData: dict = None, tablesLimitOffset: 'dict[str, tuple[int]]' = None) -> dict:
-        with await get_connection(dbType, dbPath, additionalData) as connection:
+async def get_cursor_description(cursor):
+    try:
+        return [description[0] for description in cursor.description]
+    except pyodbc.Error:
+        raise ConnectionError
+
+async def read_tables(dbType: str, dbPath: str, additionalParams: dict = None, tablesLimitOffset: 'dict[str, tuple[int]]' = None) -> dict:
+    try:
+        with await get_connection(dbType, dbPath, additionalParams) as connection:
             cursor = await get_cursor(connection)
             tasks = [
-                create_task(build_table_data(dbType, tableName, cursor, additionalData, LimitOffset))
+                create_task(build_table_data(dbType, tableName, cursor, additionalParams, LimitOffset))
                 for tableName, LimitOffset in tablesLimitOffset.items()
                 ]
             data = await gather(*tasks)
             cursor.close()
             return data
+    
+    except (ArgumentError, ConnectionError, ExecutionError, UnsupportedDBTypeError) as e:
+        raise
 
-async def get_postgresql_structure(additionalData: dict, tableName: str) -> str:
+async def get_postgresql_structure(additionalParams: dict, tableName: str) -> str:
     # Escribir guia de uso para POSTGRESQL (set en modo TRUST)
-    process = Popen(f"pg_dump -U {additionalData['user']} -t 'public.{tableName}' --schema-only {additionalData['dbName']}", stdout= PIPE, shell= True)
+    process = Popen(f"pg_dump -U {additionalParams['user']} -t 'public.{tableName}' --schema-only {additionalParams['dbName']}", stdout= PIPE, stderr= PIPE, shell= True)
+    if process.communicate()[1]:
+        raise ConnectionError(
+            "Failed to retrieve PostgreSQL database's schema, check for any incorrect arguments or enable TRUST authentication in 'pg_hba.conf'."
+            )
     return str(process.communicate()[0])
 
 async def run_SQLquery_and_get_result(query: str, cursor, *params) -> list:
-    if params:
-        cursor.execute(query, params)
-    else:
-        cursor.execute(query)
+    try:
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+    
+    except (pyodbc.ProgrammingError, pyodbc.Error) as e:
+        if isinstance(e, pyodbc.ProgrammingError):
+            raise ExecutionError("Failed to execute query; check for any missing/incorrect arguments (table name, parameters, ...).")
+        else:
+            raise ConnectionError("Connection with database at {host}:{port} (`{dbName}`) has been lost.")
     
     return cursor.fetchall()
 
-async def build_table_data(dbType: str, tableName: str, cursor, additionalData: dict = None, limitOffset: 'tuple[int]' = None) -> 'dict[str, tuple[list]]':
-   
-    if dbType != "postgresql":
-        structQuery = SQLDbStructureQueries[dbType].format(tableName)
-        tableSQL = await run_SQLquery_and_get_result(structQuery, cursor)
-    else:
-        tableSQL = await get_postgresql_structure(additionalData, tableName)
+async def build_table_data(dbType: str, tableName: str, cursor, additionalParams: dict = None, limitOffset: 'tuple[int]' = None) -> 'dict[str, tuple[list]]':
+    try:
+        if dbType != "postgresql":
+            structQuery = SQLDbStructureQueries[dbType].format(tableName)
+            tableSQL = await run_SQLquery_and_get_result(structQuery, cursor)
+        else:
+            tableSQL = await get_postgresql_structure(additionalParams, tableName)
         
-    dataQuery = SQLdataQueries[dbType].format(tableName)
-        
-    if limitOffset:
-        dataQuery += " LIMIT ? OFFSET ?"
-        if limitOffset[1] is None:
-            limitOffset = (limitOffset[0], 0)
-        tableData = await run_SQLquery_and_get_result(dataQuery, cursor, limitOffset[0], limitOffset[1])
-    else:
-        tableData = await run_SQLquery_and_get_result(dataQuery, cursor)
+        dataQuery = SQLdataQueries[dbType].format(tableName)
+            
+        if limitOffset:
+            dataQuery += " LIMIT ? OFFSET ?"
+            if limitOffset[1] is None:
+                limitOffset = (limitOffset[0], 0)
+                tableData = await run_SQLquery_and_get_result(dataQuery, cursor, limitOffset[0], limitOffset[1])
+            else:
+                tableData = await run_SQLquery_and_get_result(dataQuery, cursor)
     
-    cols = tuple([description[0] for description in cursor.description])
-    tableData.insert(0, cols)
+        cols = tuple(await get_cursor_description(cursor))
+        tableData.insert(0, cols)
     
+    except (ExecutionError, ConnectionError) as e:
+        if isinstance(e, ConnectionError):
+            e.args = (e.args[0].format(**additionalParams),)
+        raise
+
     return {
         tableName: (tableData, tableSQL)
     }
 
 # NoSQL
 
-async def get_mongo_client(additionalData: dict = None) -> pymongo.MongoClient:
-    return pymongo.MongoClient(connStrs["mongodb"].format(**additionalData))
+async def get_mongo_client(additionalParams: dict = None) -> pymongo.MongoClient:
+    return pymongo.MongoClient(connStrs["mongodb"].format(**additionalParams))
 
-async def read_collections(additionalData: dict = None, collectionLimitSkip: 'dict[str, tuple[int]]' = None) -> dict:
-        with await get_mongo_client(additionalData) as client:
+async def read_collections(additionalParams: dict = None, collectionLimitSkip: 'dict[str, tuple[int]]' = None) -> dict:
+        with await get_mongo_client(additionalParams) as client:
             tasks = [
-                create_task(build_collection_data(collectionName, client, additionalData, LimitSkip))
+                create_task(build_collection_data(collectionName, client, additionalParams, LimitSkip))
                 for collectionName, LimitSkip in collectionLimitSkip.items()
                 ]
             data = await gather(*tasks)
@@ -91,8 +133,8 @@ async def run_mongoquery_and_get_result(queryElems: list, collectionObject, *par
     queryObj = f"collectionObject" + "".join(mongodbAvailableQueryElems[elem] for elem in queryElems).format(*params)
     return eval(queryObj)
 
-async def build_collection_data(collectionName: str, client: pymongo.MongoClient, additionalData: dict = None, limitSkip: 'tuple[int]' = None) -> 'dict[str, list]':
-    collection = client[additionalData['dbName']][collectionName]
+async def build_collection_data(collectionName: str, client: pymongo.MongoClient, additionalParams: dict = None, limitSkip: 'tuple[int]' = None) -> 'dict[str, list]':
+    collection = client[additionalParams['dbName']][collectionName]
 
     if limitSkip:
         if limitSkip[1] is None:
@@ -106,4 +148,4 @@ async def build_collection_data(collectionName: str, client: pymongo.MongoClient
     }
 
 if __name__ == "__main__":
-    run(read_tables("mysql", "/home/brunengo/Escritorio/Proshecto/northwind.db", additionalData= {"user": "DBDummy", "password": "sql", "host": "localhost", "dbName": "classicmodels", "port": 3306}, tablesLimitOffset= {"customers": (20, None)}))
+    run(read_tables("postgresql", "/home/brunengo/Escritorio/Proshecto/northwind.db", additionalParams= {"user": "dbdummy", "password": "sql", "host": "localhost", "dbName": "dvdrental", "port": 5433}, tablesLimitOffset= {"customers": (20, None)}))
---
+++ final/client/exceptions.py
@@ -0,0 +1,11 @@
+class ConnectionError(Exception):
+    pass
+
+class UnsupportedDBTypeError(Exception):
+    pass
+
+class ExecutionError(Exception):
+    pass
+
+class ArgumentError(Exception):
+    pass
