5d56586c6947bf3f9488644d421321638372950c
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2023-03-26 22:30:07 -0300

    Added simple create statement migration with regex

--- final/server/funcs.py
+++ final/server/funcs.py
@@ -1,7 +1,43 @@
 from celery import Celery
+from typing import Literal
+from re import sub
 
 app = Celery("funcs", broker="redis://localhost", backend="redis://localhost:6379")
 
+# Probar procesamiento con unidades de diferentes tamaños (diferentes chunks)
+
+@app.task
+def process_information():
+    pass
+
+@app.task
+def convert_table_create_statement_to_sqlite3(createStatement: str, originDBType: Literal["mysql", "postgresql", "mongodb"], tableName: str):
+    
+    # Adaptar cuestiones sintácticas básicas (caracteres para delimitar literales, información adicional innecesaria, etc)
+    
+    if originDBType == "mysql":
+        substitutions = {
+            r'(?<!FOREIGN|PRIMARY) KEY .+,\n': "",
+            r'ENGINE=.+': "",
+            r'\s\`': " [",
+            r'\`\s': "] ",
+            r'\(\`': "([",
+            r'\`\)': "])"
+        }
+        
+    elif originDBType == "postgresql":
+        substitutions = {
+            r'CREATE TABLE .*?\..*? \(': f"CREATE TABLE [{tableName}] (",
+            r'\\n    (?!ADD CONSTRAINT)': "\\n    [",
+            r'(\n    \[[\w\s]*?) ': r"\1] ",
+            r'"': ""
+        }
+    
+    for pattern, substitution in substitutions.items():
+        createStatement = sub(pattern, substitution, createStatement)
+    
+    return createStatement
+        
 @app.task
 def convertSQLtoNoSQL(tableData, originType, destinationType):
     pass
@@ -9,3 +45,6 @@ def convertSQLtoNoSQL(tableData, originType, destinationType):
 @app.task
 def convertNoSQLtoSQL(data, originType, destinationType):
     pass
+
+if __name__ == "__main__":
+    app.start()
