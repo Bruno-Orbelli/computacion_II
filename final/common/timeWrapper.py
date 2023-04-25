c628a3c3ad58d8108e7150ce83954dd5d53e7623
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2023-04-24 22:13:10 -0300

    timeWrapper now sync

--- final/common/timeWrapper.py
+++ final/common/timeWrapper.py
@@ -3,9 +3,9 @@ from time import perf_counter
 
 def time_excecution(func: 'function'):
     @wraps(func)
-    async def wrap(*args, **kwargs):
+    def wrap(*args, **kwargs):
         start = perf_counter()
-        result = await func(*args, **kwargs)
+        result = func(*args, **kwargs)
         end = perf_counter()
         return result, end - start
     return wrap
