7e2200d5c0a0b5ba72dc3216834dc95b650c4e97
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2023-05-14 22:55:40 -0300

    Minor typing changes to wrappers

---
+++ final/common/wrappers.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from time import perf_counter
+
+def time_excecution(func: 'function') -> 'function':
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+        return result, end - start
+    return wrap
